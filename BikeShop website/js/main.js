9af141c2b299ae8ad834d40b3e51b2f52cf10005
Igor Krasnov <krasnov.igor2@gmail.com>
2021-03-17 16:42:47 +0200

    Added Autocomplete search form on web site

--- BikeShop website/js/main.js
+++ BikeShop website/js/main.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
-  
-  $("#calendar").datepicker();
+	
+	$("#calendar").datepicker();
 
-  var availableTags = [
-      "Cruiser",
-      "Brera Blue",
-      "XDS Retro Alloy",
-      "Cafe Racer",
-      "Retro 5.6",
-      "Liv Beliv F 2021",
-  ];
-  
-  $( "#tags" ).autocomplete({
-      source: availableTags
-  });
-  
+	var availableTags = [];
+	var title_array = Array.prototype.slice.call(document.querySelectorAll('.item__header h2'));
+	
+	function addTags() {
+		for (let i = 0; i < title_array.length; i++) {
+			availableTags.push(title_array[i].textContent);
+		}
+	}
+
+	addTags();
+
+	$( "#tags" ).autocomplete({
+			source: availableTags
+	});
+	
 });
